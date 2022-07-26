fd5813069ab87860123c898f84f3ccde087b04d0
adallon <antoinedallon@gmail.com>
2022-07-26 23:52:15 +0200

    cleaning

--- lib/hashmemory.mli
+++ lib/hashmemory.mli
@@ -2,4 +2,3 @@ type t
 val create : int -> t
 val add_group: t -> int -> Geometry.Point.t list -> int -> unit
 val get_corner: t -> int -> Geometry.Direction.t -> Geometry.Hand.t -> int * Geometry.Point.t
-val get_group_size : t -> int -> int
