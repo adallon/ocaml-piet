d756ddfd0d13e253cef93390848c4791cea62687
adallon <antoinedallon@gmail.com>
2022-07-26 23:56:00 +0200

    cleaning

--- lib/machine.mli
+++ lib/machine.mli
@@ -1,13 +1,3 @@
 type t
 
-(*
-
-val next_state : t -> int -> Instructions.t -> t
-(* machine, blocksize, instruction *)
-
-val get_direction: t -> Geometry.Direction.t
-val get_hand     : t -> Geometry.Hand.t
-val set          : Geometry.Direction.t -> Geometry.Hand.t -> t -> t
-*)
-
 val interpreter: Program.t -> unit
